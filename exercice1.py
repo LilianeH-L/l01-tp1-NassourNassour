4b5180287d4a22a02cc4b65b9b83fc95d0a8d617
LilianeH-L <90001226+LilianeH-L@users.noreply.github.com>
2021-09-14 10:59:41 -0400

    asdf

--- exercice1.py
+++ exercice1.py
@@ -2,7 +2,7 @@ def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
 
-    resultat =
+    resultat = asdfsaf
     return resultat
 
 if __name__ == '__main__':
